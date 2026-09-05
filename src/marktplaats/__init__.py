0fd2a1dcac782e9b83dc3a2a6615aee3f60a9146
Erwin Kooij <erwin@ekweb.nl>
2026-09-05 03:37:44 +0200

    Rename import paths from marktplaats to 2dehandsbe

--- src/marktplaats/__init__.py
+++ src/marktplaats/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from marktplaats.categories import (
+from 2dehandsbe.categories import (
     L1Category as L1Category,
     L2Category as L2Category,
     category_from_name as category_from_name,
@@ -9,14 +9,14 @@ from marktplaats.categories import (
     get_l2_categories_by_parent as get_l2_categories_by_parent,
     get_subcategories as get_subcategories,
 )
-from marktplaats.models import (
+from 2dehandsbe.models import (
     Listing as Listing,
     ListingFirstImage as ListingFirstImage,
     ListingLocation as ListingLocation,
     ListingSeller as ListingSeller,
     PriceType as PriceType,
 )
-from marktplaats.query import (
+from 2dehandsbe.query import (
     BadStatusCodeError as BadStatusCodeError,
     Condition as Condition,
     JSONDecodeError as JSONDecodeError,
@@ -24,4 +24,4 @@ from marktplaats.query import (
     SortBy as SortBy,
     SortOrder as SortOrder,
 )
-from marktplaats.seller_query import SellerQuery as SellerQuery
+from 2dehandsbe.seller_query import SellerQuery as SellerQuery
